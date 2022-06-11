a30822e56223260b5c00c53e67c820af32f600e8
marleigh <volcana@gmail.com>
2022-06-11 14:47:07 -0700

    creating object

--- public/index.js
+++ public/index.js
@@ -4,4 +4,14 @@ let strArr = ["1", "55"];
 if (age < 50)
     age += 10;
 console.log(age);
+function test(id) {
+    return id;
+}
+let cat = {
+    name: "Tali",
+    age: 10,
+    meow: () => {
+        console.log('meow');
+    }
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,32 @@ if (age < 50)
     age += 10;
     console.log(age);
 
+function test(id: number): number {
+    return id
+}
+
+// let cat: {
+//     name: string,
+//     age: number,
+//     meow: () => void
+// } = {
+//     name: "Tali",
+//     age: 10,
+//     meow: () => {
+//         console.log('meow');
+//     }
+// }
+
+type Cat = {
+    name: string,
+    age: number,
+    meow: () => void
+} 
+
+let cat: Cat = {
+    name: "Tali",
+    age: 10,
+    meow: () => {
+        console.log('meow');
+    }
+} 

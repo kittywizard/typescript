4f329e04231550714d4ae37128c3b0a33955a48b
marleigh <volcana@gmail.com>
2022-06-11 10:40:54 -0700

    learning configuration

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
-let message = 'Hello World';
-console.log(message);
+let age = 20;
+let arr = [1, 2];
+let strArr = ["1", "55"];
+if (age < 50)
+    age += 10;
+console.log(age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-let message: string = 'Hello World';
-console.log(message);
+let age: number = 20;
+let arr: number[] = [1,2];
+let strArr: string[] =["1", "55"]; 
+
+if (age < 50)
+    age += 10;
+    console.log(age);
+
